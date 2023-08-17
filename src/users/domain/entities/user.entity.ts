a3536ec537bd1c40106fa88e9c8f25de44b09bac
Erick Germani <erickgermani13@gmail.com>
2023-08-16 23:29:18 -0300

    Criando a classe UserValidator

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity';
+import { UserValidatorFactory } from '../validators/user.validator';
+import { Validator } from 'class-validator';
 
 export type UserProps = {
   name: string;
@@ -12,15 +14,18 @@ export class UserEntity extends Entity<UserProps> {
     public readonly props: UserProps,
     id?: string,
   ) {
+    UserEntity.validate(props);
     super(props, id);
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
   update(value: string): void {
+    UserEntity.validate({ ...this.props, name: value });
     this.name = value;
   }
 
   updatePassword(value: string): void {
+    UserEntity.validate({ ...this.props, password: value });
     this.password = value;
   }
 
@@ -46,5 +51,11 @@ export class UserEntity extends Entity<UserProps> {
 
   get createdAt() {
     return this.props.createdAt;
+  }
+
+  static validate(props: UserProps) {
+    const validator = UserValidatorFactory.create();
+
+    validator.validate(props);
   }
 }
