caa584fe81942191baa84c233ca4dd99adbfe6b8
Erick Germani <erickgermani13@gmail.com>
2023-08-18 19:56:34 -0300

    Criando entity validation error

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity';
 import { UserValidatorFactory } from '../validators/user.validator';
+import { EntityValidationError } from '@/shared/domain/errors/validation-error';
 
 export type UserProps = {
   name: string;
@@ -54,7 +55,8 @@ export class UserEntity extends Entity<UserProps> {
 
   static validate(props: UserProps) {
     const validator = UserValidatorFactory.create();
+    const isValid = validator.validate(props);
 
-    validator.validate(props);
+    if (!isValid) throw new EntityValidationError(validator.errors);
   }
 }
