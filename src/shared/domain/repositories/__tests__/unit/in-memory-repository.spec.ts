09626dc7c9d98a7f990033c0e4ebd81eeb5988a8
Erick Germani <erickgermani13@gmail.com>
2023-08-19 09:08:01 -0300

    Concluindo testes unitários do InMemoryRepository

--- src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-repository.spec.ts
@@ -32,7 +32,7 @@ describe('InMemoryRepository unit tests', () => {
     );
   });
 
-  it('Should find a entity by id', async () => {
+  it('Should find an entity by id', async () => {
     const entity = new StubEntity({ name: 'test name', price: 100 });
 
     await sut.insert(entity);
@@ -40,5 +40,53 @@ describe('InMemoryRepository unit tests', () => {
     const result = await sut.findById(entity.id);
 
     expect(entity.toJSON()).toStrictEqual(result.toJSON());
+  });
+
+  it('Should returns all entities', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 100 });
+
+    await sut.insert(entity);
+
+    const result = await sut.findAll();
+
+    expect([entity]).toStrictEqual(result);
+  });
+
+  it('Should throw error on update when entity not found', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 100 });
+
+    expect(sut.update(entity)).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('Should update an entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 100 });
+
+    await sut.insert(entity);
+
+    const entityUpdated = new StubEntity(
+      { name: 'updatedName', price: 50 },
+      entity._id,
+    );
+
+    await sut.update(entityUpdated);
+
+    expect(entityUpdated.toJSON()).toStrictEqual(sut['items'][0].toJSON());
+  });
+
+  it('Should throw error when entity not found', async () => {
+    expect(sut.delete('fakeId')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('Should delete an entity', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 100 });
+
+    await sut.insert(entity);
+    await sut.delete(entity._id);
+
+    expect(sut['items']).toHaveLength(0);
   });
 });
