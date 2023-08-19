08606630ebdcd54545d40eb131e258c87db1ada5
Erick Germani <erickgermani13@gmail.com>
2023-08-19 16:46:42 -0300

    Criando testes unitários para SearchableRepositoryContracts

---
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -0,0 +1,59 @@
+import { SearchParams } from '../../searchable-repository-contracts';
+
+describe('Searchable Repository unit tests', () => {
+  describe('SearchParams tests', () => {
+    it('page prop', () => {
+      const sut = new SearchParams();
+
+      expect(sut.page).toBe(1);
+
+      const params = [
+        { page: null as any, expected: 1 },
+        { page: undefined, expected: 1 },
+        { page: '', expected: 1 },
+        { page: 'test', expected: 1 },
+        { page: 0, expected: 1 },
+        { page: -1, expected: 1 },
+        { page: 5.5, expected: 1 },
+        { page: true, expected: 1 },
+        { page: false, expected: 1 },
+        { page: {}, expected: 1 },
+        { page: [], expected: 1 },
+        { page: 1, expected: 1 },
+        { page: 2, expected: 2 },
+      ];
+
+      params.forEach(i => {
+        expect(new SearchParams({ page: i.page }).page).toBe(i.expected);
+      });
+    });
+
+    it('perPage prop', () => {
+      const sut = new SearchParams();
+
+      expect(sut.perPage).toBe(15);
+
+      const params = [
+        { perPage: null as any, expected: 15 },
+        { perPage: undefined, expected: 15 },
+        { perPage: '', expected: 15 },
+        { perPage: 'test', expected: 15 },
+        { perPage: 0, expected: 15 },
+        { perPage: -1, expected: 15 },
+        { perPage: 5.5, expected: 15 },
+        { perPage: true, expected: 15 },
+        { perPage: false, expected: 15 },
+        { perPage: {}, expected: 15 },
+        { perPage: [], expected: 15 },
+        { perPage: 1, expected: 1 },
+        { perPage: 16, expected: 16 },
+      ];
+
+      params.forEach(i => {
+        expect(new SearchParams({ perPage: i.perPage }).perPage).toBe(
+          i.expected,
+        );
+      });
+    });
+  });
+});
