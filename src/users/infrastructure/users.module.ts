cc7d11068fd96ab2d492cc98132cae1b1220f909
Erick Germani <erickgermani13@gmail.com>
2023-09-02 19:53:24 -0300

    Configurando o UsersController

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import SigninUseCase from '../application/usecases/signin.usecase';
 import UserInMemoryRepository from './database/in-memory/repositories/user-in-memory.repository';
@@ -16,7 +15,6 @@ import DeleteUserUseCase from '../application/usecases/delete-user.usecase';
 @Module({
   controllers: [UsersController],
   providers: [
-    UsersService,
     {
       provide: 'UserRepository',
       useClass: UserInMemoryRepository,
