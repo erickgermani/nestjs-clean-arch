ff142bf5ed8bab3ab8cdcfbb7708fc3fcb9b5d40
Erick Germani <erickgermani13@gmail.com>
2023-09-22 19:36:04 -0300

    User presenter class unit tests

---
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -0,0 +1,41 @@
+import { instanceToPlain } from 'class-transformer';
+import UserPresenter from '../../user.presenter';
+import { create } from 'domain';
+
+describe('UserPresenter unit tests', () => {
+  let sut: UserPresenter;
+
+  const createdAt = new Date();
+
+  const props = {
+    id: 'beca4dc4-399e-4c4b-b6e5-bb032559fb95',
+    name: 'test name',
+    email: 'test@mail.com',
+    password: 'fake',
+    createdAt,
+  };
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
+
+  describe('constructor', () => {
+    it('should be defined', () => {
+      expect(sut.id).toEqual(props.id);
+      expect(sut.name).toEqual(props.name);
+      expect(sut.email).toEqual(props.email);
+      expect(sut.createdAt).toEqual(props.createdAt);
+    });
+  });
+
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut);
+
+    expect(output).toEqual({
+      id: 'beca4dc4-399e-4c4b-b6e5-bb032559fb95',
+      name: 'test name',
+      email: 'test@mail.com',
+      createdAt: createdAt.toISOString(),
+    });
+  });
+});
