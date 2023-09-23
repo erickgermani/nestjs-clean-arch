ecaaf6e3f13d42072d41990551aebe1ad97de4ac
Erick Germani <erickgermani13@gmail.com>
2023-09-23 14:03:03 -0300

    NotFoundError e2e tests

--- src/shared/infrastructure/exception-filters/conflict-error/__tests__/unit/conflict-error.filter.e2e-spec.ts
+++ src/shared/infrastructure/exception-filters/conflict-error/__tests__/unit/conflict-error.filter.e2e-spec.ts
@@ -26,10 +26,6 @@ describe('ConflictErrorFilter (e2e)', () => {
     await app.init();
   });
 
-  afterAll(async () => {
-    await module.close();
-  });
-
   it('should be defined', () => {
     expect(new ConflictErrorFilter()).toBeDefined();
   });
