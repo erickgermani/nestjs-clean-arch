140db3b287140942e7bb80c4ccb933fa63d504d2
Erick Germani <erickgermani13@gmail.com>
2023-09-24 19:31:23 -0300

    InvalidCredentialsErrorFilter e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,10 @@ import { UsersModule } from './users/infrastructure/users.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
 import { PrismaService } from './shared/infrastructure/database/prisma/prisma.service';
 import { AuthModule } from './auth/infrastructure/auth.module';
-import { FilterService } from './shared/infrastructure/exception-filters/invalid-credentials-error/filter/filter.service';
 
 @Module({
   imports: [EnvConfigModule, UsersModule, DatabaseModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, FilterService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
