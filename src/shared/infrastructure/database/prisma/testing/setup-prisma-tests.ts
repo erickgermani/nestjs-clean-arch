c96229a3d72b3d70671bcf7f91689d9f7e685232
Erick Germani <erickgermani13@gmail.com>
2023-09-24 19:19:47 -0300

    UsersController e2e tests - login method

--- src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
+++ src/shared/infrastructure/database/prisma/testing/setup-prisma-tests.ts
@@ -1,5 +1,7 @@
 import { execSync } from 'node:child_process';
 
 export function setupPrismaTests() {
-  execSync('npx dotenv-cli -e .env.test -- npx prisma migrate deploy');
+  execSync(
+    'npx dotenv-cli -e .env.test -- npx prisma migrate deploy --schema=./src/shared/infrastructure/database/prisma/schema.prisma',
+  );
 }
