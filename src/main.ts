8784224dc0787f8d695aa0e81f18565188e11c5f
Erick Germani <erickgermani13@gmail.com>
2023-09-24 21:12:11 -0300

    Installing swagger lib

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,30 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Node.js Rest API - NestJs')
+    .setDescription('The Node.js Rest API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Informar o JWT para autorizar o acesso',
+      name: 'Authorization',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   applyGlobalConfig(app);
 
