e5406980f27edfdbd49bc1e1b1daf50f22168541
Erick Germani <erickgermani13@gmail.com>
2023-09-24 21:19:30 -0300

    Refactoring DTOs with the documentation

--- src/users/infrastructure/dtos/list-users.dto.ts
+++ src/users/infrastructure/dtos/list-users.dto.ts
@@ -1,20 +1,30 @@
 import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contracts';
 import ListUsersUseCase from '@/users/application/usecases/list-users.usecase';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 
 export class ListUsersDto implements ListUsersUseCase.Input {
+  @ApiPropertyOptional({ description: 'Page number that will returned' })
   @IsOptional()
   page?: number;
 
+  @ApiPropertyOptional({ description: 'Quantity of registers per page' })
   @IsOptional()
   perPage?: number;
 
+  @ApiPropertyOptional({
+    description: 'Defined column to order data: "name" or "createdAt"',
+  })
   @IsOptional()
   sort?: string;
 
+  @ApiPropertyOptional({
+    description: 'Defined direction to order data: "asc" or "desc"',
+  })
   @IsOptional()
   sortDir?: SortDirection;
 
+  @ApiPropertyOptional({ description: 'Defined filter to search data' })
   @IsOptional()
   filter?: string;
 }
--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,13 +1,16 @@
 import UpdatePasswordUseCase from '@/users/application/usecases/update-password.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUseCase.Input, 'id'>
 {
+  @ApiProperty({ description: 'New user password' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ description: 'Current user password' })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
